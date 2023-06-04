fcdccba3331887ce7e633940b5c4b6dbab82a627
randall.erasmus <randall.erasmus@interfront.co.za>
2023-06-04 02:42:32 +0200

    Initial commit

---
+++ src/main/java/com/example/droppingstorebackend/DroppingstorebackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.droppingstorebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroppingstorebackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroppingstorebackendApplication.class, args);
+	}
+
+}
